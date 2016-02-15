0b7473a4652581aeb05745df1b47e4b98c7ffe86
Michal <michal.macejko@3scale.net>
2016-02-15 14:46:08 +0100

    Use let instead of var

--- src/lazy_loader.js
+++ src/lazy_loader.js
@@ -2,15 +2,15 @@ import fetchJsonp from 'fetch-jsonp'
 
 export function lazyLoad() {
 
-  var gists     = Array.from(document.getElementsByTagName('gist'))
-  var loadedCss = {}
+  let gists     = Array.from(document.getElementsByTagName('gist'))
+  let loadedCss = {}
 
   gists.forEach(function(gist) {
-    
+
     if (!gist.hasAttribute('data-src')) { return; }
 
-    var url     = gist.getAttribute('data-src')
-    var jsonUrl = url.replace(/\.js\b/, '.json')
+    let url     = gist.getAttribute('data-src')
+    let jsonUrl = url.replace(/\.js\b/, '.json')
 
     setTimeout(function(){
 
@@ -20,11 +20,11 @@ export function lazyLoad() {
         }).then(function(json) {
 
           window.requestAnimationFrame(function(){
-            var gistDiv = document.createElement('div')
+            let gistDiv = document.createElement('div')
             gistDiv.innerHTML = json.div
 
             if(!loadedCss[json.stylesheet]) {
-              var stylesheet = document.createElement('link')
+              let stylesheet = document.createElement('link')
               stylesheet.setAttribute('rel', 'stylesheet')
               stylesheet.setAttribute('type', 'text/css')
               stylesheet.setAttribute('href', json.stylesheet)
